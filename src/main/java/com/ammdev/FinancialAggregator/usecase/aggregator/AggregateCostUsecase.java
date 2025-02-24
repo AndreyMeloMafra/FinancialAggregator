e264022ca8685633426014cab3177bcec8954110
andreymafra <andreymelo44@gmail.com>
2025-02-24 08:55:56 -0300

    Criação do AggregateCostUsecase e seu teste unitário

--- src/main/java/com/ammdev/FinancialAggregator/usecase/aggregator/AggregateCostUsecase.java
+++ src/main/java/com/ammdev/FinancialAggregator/usecase/aggregator/AggregateCostUsecase.java
@@ -1,9 +1,11 @@
 package com.ammdev.FinancialAggregator.usecase.aggregator;
 
 import com.ammdev.FinancialAggregator.domain.AggregatorResponse;
+import com.ammdev.FinancialAggregator.domain.Cost;
+import com.ammdev.FinancialAggregator.domain.Period;
 
-import java.time.Period;
+import java.util.List;
 
 public interface AggregateCostUsecase {
-    AggregatorResponse execute(String userId, Period period);
+    AggregatorResponse execute(List<Cost> costs, Period period);
 }
