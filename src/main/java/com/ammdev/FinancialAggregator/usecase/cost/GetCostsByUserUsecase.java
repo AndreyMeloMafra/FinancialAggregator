b9563245200e774ff8029297d16e79c8a305cddb
andreymafra <andreymelo44@gmail.com>
2025-02-24 08:53:57 -0300

    Remoção de usecase não utilizado

--- src/main/java/com/ammdev/FinancialAggregator/usecase/cost/GetCostsByUserUsecase.java
+++
@@ -1,9 +0,0 @@
-package com.ammdev.FinancialAggregator.usecase.cost;
-
-import com.ammdev.FinancialAggregator.domain.Cost;
-
-import java.util.List;
-
-public interface GetCostsByUserUsecase {
-    List<Cost> getCostsByUser(String userId);
-}
